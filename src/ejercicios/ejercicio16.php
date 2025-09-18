19492bf5791a3d35bf80b37d88c883bf3eb648c4
nexphernandez <nicolaseh3b1112@gmail.com>
2025-09-18 15:51:45 +0100

    Update

---
+++ src/ejercicios/ejercicio16.php
@@ -0,0 +1,9 @@
+<?php
+$numero = (int) readline("Dime un numero para verificar si es un numero perfecto ");
+$suma = 0;
+for ($i=1; $i <$numero ; $i++) { 
+    if($numero %i == 0){
+        
+    }
+}
+?>
